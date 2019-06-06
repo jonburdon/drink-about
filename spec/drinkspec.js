7f4a17cd00398b783ec67f5f2bab9e564dfef6a6
Ubuntu <ubuntu@ip-172-31-94-205.ec2.internal>
2019-06-06 22:01:42 +0000

    Checks completed for all ages

--- spec/drinkspec.js
+++ spec/drinkspec.js
@@ -16,5 +16,22 @@ describe("What Can I drink function", function() {
             expect(result).toBe("Drink Fanta");
         });
     });
-
+    describe("Checks Age", function() {
+        it("should return drink toddy when called as whatCanIDrink(13)", function() {
+            var result = whatCanIDrink(20)
+            expect(result).toBe("Drink Beer");
+        });
+    });
+    describe("Checks Age", function() {
+        it("should return drink toddy when called as whatCanIDrink(13)", function() {
+            var result = whatCanIDrink(129)
+            expect(result).toBe("Drink Whiskey");
+        });
+    });
+    describe("Checks Age", function() {
+        it("should return drink toddy when called as whatCanIDrink(13)", function() {
+            var result = whatCanIDrink(131)
+            expect(result).toBe("Sorry. I can't tell what drink because that age is incorrect!");
+        });
+    });
 });
